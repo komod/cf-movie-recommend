166cf73545e881e2127c168a259ba364ed140f96
Komod Wang <komod.Wang@gmail.com>
2017-03-24 17:26:31 +0800

    Check authorized user and recommend according to user id

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,30 @@
 import logging
 
 # flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import flask_cors
 
 # Google
 from google.cloud import datastore
 from google.cloud.exceptions import ServiceUnavailable
+import google.auth.transport.requests
+import google.oauth2.id_token
 
 # data
 import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances
 
 RATING_KIND = 'Rating'
+HTTP_REQUEST = google.auth.transport.requests.Request()
 
 app = Flask(__name__)
 flask_cors.CORS(app)
 client = datastore.Client('cf-mr-service')
 
 general_recommendation = []
+user_rating = None
+user_rating_split_size = 0
+user_prediction = None
 
 @app.route('/')
 def hello():
@@ -25,11 +32,26 @@ def hello():
 
 @app.route('/movie/api/v1.0/recommendation', methods=['GET'])
 def recommend_movies():
+    user_info = get_user_info()
+    
     movie_list = []
-    for m in general_recommendation:
-        movie_list.append({
-            'movie_id': m[2]
-            })
+
+    if user_info['index'] < 0:
+        for m in general_recommendation:
+            movie_list.append({
+                'movie_id': m[2]
+                })        
+    elif user_info['index'] < user_rating.shape[0]:
+        movies = []
+        for i in xrange(user_rating.shape[1]):
+            if user_rating[user_info['index']][i] == 0:
+                movies.append((user_prediction[user_info['index']][i], i))
+
+        movies.sort(reverse=True)        
+        for m in movies:
+            movie_list.append({
+                'movie_id': m[1]
+                })
 
     return jsonify(movie_list)
 
@@ -47,13 +69,14 @@ def log_info(msg):
 def load_data():
     log_info('load data')
 
+    global user_rating_split_size
     entity = retry_get_entity(client.key('Config', 'v1.0'))
     if entity is not None:
         user_rating_split_size = entity.get('user_rating_split_size', 0)
 
+    global user_rating
     query = client.query(kind=RATING_KIND)
     rating = list(query.fetch())
-    user_rating = None
     for entity in rating:
         arr = np.fromstring(entity.get('data_str', ''), dtype=entity.get('dtype', 'uint8')).reshape(entity.get('rows', 1), entity.get('cols', 1))
         if user_rating is not None:
@@ -64,6 +87,9 @@ def load_data():
     if user_rating is not None:
         log_info('Number of users = ' + str(user_rating.shape[0]) + ' | Number of movies = ' + str(user_rating.shape[1]))
 
+    predict_all()
+    
+    global movie_ratings
     movie_ratings = [[0.0, 0, i] for i in xrange(user_rating.shape[1])]
     for movie_id in xrange(len(movie_ratings)):
         ratings = user_rating[:, movie_id:movie_id + 1]
@@ -83,6 +109,63 @@ def retry_get_entity(key):
     except ServiceUnavailable:
         entity = client.get(key=key)
     return entity
+
+def predict_all():
+    user_similarity = pairwise_distances(user_rating, metric='cosine')
+
+    global user_prediction
+    user_prediction = predict(user_rating, user_similarity)
+
+def predict(ratings, similarity):
+    mean_user_rating = ratings.mean(axis=1)
+    #You use np.newaxis so that mean_user_rating has same format as ratings
+    ratings_diff = (ratings - mean_user_rating[:, np.newaxis]) 
+    return mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+
+def get_user_info():
+    id_token = request.headers['Authorization'].split(' ').pop()
+    claims = None
+    if id_token != 'null' and id_token != 'undefined':
+        claims = google.oauth2.id_token.verify_firebase_token(id_token, HTTP_REQUEST)
+
+    email = ''
+    index = -1
+    if claims is not None:
+        email = claims['email']
+        entity = retry_get_entity(client.key('User', email))
+        if entity is not None:
+            index = entity.get('user_index', -1)
+        elif user_rating_split_size > 0:
+            # authorized user without index, add to rating matrix and db
+            global user_rating
+            index = user_rating.shape[0]
+            user_rating = np.append(user_rating, np.zeros((1, user_rating.shape[1]), dtype='uint8'), axis = 0)
+
+            data_set_index = index / user_rating_split_size
+            key = client.key(RATING_KIND, str(data_set_index))
+            entity = retry_get_entity(key)
+            if entity is None:
+                entity = datastore.Entity(key=key, exclude_from_indexes=['data_str'])
+            sub_arr = user_rating[data_set_index * user_rating_split_size : user_rating.shape[0]]
+            entity['rows'] = sub_arr.shape[0]
+            entity['cols'] = sub_arr.shape[1]
+            entity['dtype'] = str(sub_arr.dtype)
+            entity['data_str'] = sub_arr.tostring()
+
+            user_entity = datastore.Entity(key=client.key('User', email))
+            user_entity['user_index'] = index
+            try:
+                with client.transaction():
+                    client.put_multi([entity, user_entity])
+            except:
+                user_rating = np.delete(user_rating, user_info['index'], 0)
+                index = -1
+            else:
+                predict_all()
+    return {
+        'email': email,
+        'index': index
+        }
 
 load_data()
 if __name__ == '__main__':
