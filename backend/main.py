cb7035ed310da7aa3af88d43db73f01f9bb6a2ed
Komod Wang <komod.Wang@gmail.com>
2017-03-24 16:16:55 +0800

    Add basic files for backend flexible environment

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+import logging
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return ''
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8081, debug=False)
